8f8aa2ba9325de7a799ad8bc0208852ea9662dc0
Surapitch Kongthong <140515213+A70III@users.noreply.github.com>
2023-09-14 15:54:03 +0700

    FormatCode

--- RadarGraph.py
+++ RadarGraph.py
@@ -1,51 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
 class RadarGraph:
     
     def __init__(self) -> None:
         pass
         
     def PlotRadar(self, UniversityList, ScoreList):
-        import numpy as np
-        import matplotlib.pyplot as plt
-        import os
-        
         self.UniversityList = UniversityList
         self.ScoreList = ScoreList
 
-        # Labels for the radar chart axes (หกด้าน)
+        # ประกาศ Label ของแต่ละแกนของ Radar Chart
         Majors = ['Language', 'Thinking', 'Arts and Society', 'Science', 'Mathematics', 'General']
 
-        # Colors for each sample
+        # กำหนดสีสำหรับแต่ละตัวอย่าง
         colors = ['magenta', 'blue', 'green', 'red', 'orange', 'purple', 'yellow', 'cyan', 'brown', 'black']  # จำกัดไว้ให้เท่านั้น
 
-        # Create a figure and subplot
+        # สร้างกราฟและ subplot
         self.fig, ax = plt.subplots(figsize=(6, 7.5), subplot_kw={'polar': True})
-
 
         for i, sample_data in enumerate(ScoreList):
             angles = np.linspace(0, 2 * np.pi, len(Majors), endpoint=False).tolist()
-            sample_data += sample_data[:1]  # Close the plot
-            angles += angles[:1]  # Close the plot
-            countUniver = "University {}".format(i+1)
+            sample_data += sample_data[:1]  # ปิดรูปกราฟ
+            angles += angles[:1]  # ปิดรูปกราฟ
+            countUniver = "university {}".format(i+1)
 
             ax.plot(angles, sample_data, marker='o', linestyle='-', color=colors[i], linewidth=2, label=countUniver)
             ax.fill(angles, sample_data, color=colors[i], alpha=0.25)
 
-        # Set the ticks and labels for the radar chart
+        # กำหนดเส้นและป้ายชื่อของแกนใน Radar Chart
         ax.set_xticks(angles[:-1])
         ax.set_xticklabels(Majors)
 
         ax.yaxis.grid(True)
         plt.subplots_adjust(top=1.05)
 
-
-        # Add a legend
+        # เพิ่มสัญลักษณ์
         ax.legend(loc='lower center', bbox_to_anchor=(0.5, -0.39), ncol=3)
 
-        # Set the title
-        title_offset = 1.1  # You can adjust the value as needed
+        # กำหนดหัวเรื่อง
+        title_offset = 1.1  # คุณสามารถปรับค่าได้ตามที่ต้องการ
         ax.set_title("Comparison of Universities", size=15, weight='bold', y=title_offset)
 
-        # Show the plot
+        # แสดงกราฟ
         static_folder = os.path.join(os.getcwd(), 'static')
         plot_path = os.path.join(static_folder, 'radar_chart.png')
         plt.savefig(plot_path)
--- app.py
+++ app.py
@@ -1,27 +1,32 @@
+# Import ไลบรารีและโมดูลที่จำเป็น
 from flask import Flask, request, render_template, redirect
 from RadarGraph import RadarGraph
 import pandas as pd
 
+# สร้างแอพ Flask
 app = Flask(__name__)
 
+# กำหนดตัวแปรรับค่าคะแนนและชื่อมหาวิทยาลัย
 ListU_Score = []
 ListU_Name = []
+
+# สร้างอ็อบเจ็กต์ RadarGraph
 radar_obj = RadarGraph()
 
-
+# สร้างเส้นทางหน้าแรก
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# เส้นทางสำหรับรับค่าและบันทึกคะแนน
 @app.route('/', methods=['GET', 'POST'])
 def foo():
-    
-    
     ScoreGroup = []
     if 'finish' in request.form:
         return redirect('/finish_page')
     UniversityName = request.form['UniversityName']
 
+    # รับค่าคะแนนจากฟอร์มและเพิ่มลงในรายชื่อคะแนน
     LangScr = float(request.form['Language'])
     ThinkScr = float(request.form['Thinking'])
     ArtScr = float(request.form['ArtNSoci'])
@@ -36,28 +41,28 @@ def foo():
     ScoreGroup.append(MathScr)
     ScoreGroup.append(GenScr)
     ListU_Score.append(ScoreGroup)
-    print(ListU_Score)##ใส่คะแนนใน group สิบครั้งได้ละ
-    
+    print(ListU_Score)  # ใส่คะแนนในกลุ่มได้ละ
+
+    # เพิ่มชื่อมหาวิทยาลัยลงในรายการ
     ListU_Name.append(UniversityName)
     universities = ListU_Name
     print(ListU_Name)
-    
-    
-    if len(ListU_Score)>=10:
-        radar_obj.PlotRadar(ListU_Name,ListU_Score)
-        
-        return render_template('radar_image.html',universities=universities,ListU_Score=ListU_Score)
-    
-    
-    
+
+    # หากมีคะแนนมหาวิทยาลัยมากกว่าหรือเท่ากับ 10 คะแนน ก็จะพล็อตกราฟ Radar
+    if len(ListU_Score) >= 10:
+        radar_obj.PlotRadar(ListU_Name, ListU_Score)
+        return render_template('radar_image.html', universities=universities, ListU_Score=ListU_Score)
+
     return render_template('index.html')
 
+# เส้นทางสำหรับหน้าสรุปผล
 @app.route('/finish_page')
 def another_page():
     universities = ListU_Name
-    radar_obj.PlotRadar(ListU_Name,ListU_Score)
-    return render_template('radar_image.html',universities=universities,ListU_Score=ListU_Score)
+    radar_obj.PlotRadar(ListU_Name, ListU_Score)
+    return render_template('radar_image.html', universities=universities, ListU_Score=ListU_Score)
 
+# เส้นทางสำหรับรีเซ็ตค่า
 @app.route('/reset', methods=['POST'])
 def reset():
     global ListU_Score
@@ -66,8 +71,6 @@ def reset():
     ListU_Name = []
     return redirect('/')
 
-
-
-
+# รันแอพ Flask
 if __name__ == '__main__':
     app.run(debug=True)
